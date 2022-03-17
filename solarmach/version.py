4108b0fe57ace426bd25650d0827bb0a4b233356
jgieseler <jan.gieseler@utu.fi>
2022-03-17 19:32:02 +0200

    version number

--- solarmach/version.py
+++ solarmach/version.py
@@ -5,4 +5,4 @@ try:
     from setuptools_scm import get_version
     __version__ = get_version(root='..', relative_to=__file__)
 except Exception:
-    __version__ = '0.1.0'
+    __version__ = '0.1.1'
