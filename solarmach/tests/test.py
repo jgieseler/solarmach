ddf8bc114f31cfbcc2023c92f7be42a105b5745a
jgieseler <jan.gieseler@utu.fi>
2023-08-11 17:44:59 +0300

    add additional tests

--- solarmach/tests/test.py
+++ solarmach/tests/test.py
@@ -2,8 +2,17 @@ import os
 
 import astropy
 import astropy.units as u
+import matplotlib
 import numpy as np
-from solarmach import SolarMACH, print_body_list
+import pandas
+import pfsspy
+from solarmach import SolarMACH, print_body_list, get_gong_map, calculate_pfss_solution
+
+
+def test_print_body_list():
+    df = print_body_list()
+    assert isinstance(df, pandas.core.frame.DataFrame)
+    assert df['Body'].loc['PSP'] == 'Parker Solar Probe'
 
 
 def test_solarmach_initialize():
@@ -34,6 +43,14 @@ def test_solarmach_initialize():
     assert sm.coord_sys == 'Carrington'
 
 
+def test_solarmach_get_sw_speed():
+    body_list = ['STEREO-A', 'BepiColombo']
+    date = '2021-10-28 15:15:00'
+    sm = SolarMACH(date=date, body_list=body_list)
+    assert np.round(sm.coord_table[sm.coord_table['Spacecraft/Body']=='STEREO-A']['Vsw'].values[0]) == 365.0
+    assert sm.coord_table[sm.coord_table['Spacecraft/Body']=='BepiColombo']['Vsw'].values[0] == 400.0
+
+
 def test_solarmach_plot():
     body_list = ['STEREO-A']
     vsw_list = [400]
@@ -42,11 +59,31 @@ def test_solarmach_plot():
     reference_lat = 9
     reference_vsw = 400
     filename = 'test.png'
+    long_sector=[290, 328]
+    long_sector_vsw=[400, 600]
+    long_sector_color='red'
+    background_spirals=[6, 600]
 
     sm = SolarMACH(date=date, body_list=body_list, vsw_list=vsw_list, reference_long=reference_long, reference_lat=reference_lat)
     sm.plot(plot_spirals=True, plot_sun_body_line=True,
             reference_vsw=reference_vsw, transparent=False,
             show_earth_centered_coord=False, numbered_markers=True,
-            outfile=filename)
+            long_sector=long_sector, long_sector_vsw=long_sector_vsw, long_sector_color=long_sector_color,
+            background_spirals=background_spirals, outfile=filename)
 
     assert os.path.exists(os.getcwd()+os.sep+filename)
+
+
+def test_solarmach_pfss():
+    date = '2021-4-1 1:00:00'
+    body_list = ['STEREO-A']
+    vsw_list = [400]   # position-sensitive solar wind speed per body in body_list
+    sm = SolarMACH(date, body_list, vsw_list)
+    gong_map = get_gong_map(time=date, filepath=None)
+    assert isinstance(gong_map, pfsspy.map.GongSynopticMap)
+    pfss_solution = calculate_pfss_solution(gong_map=gong_map, rss=2.5)
+    assert isinstance(pfss_solution, pfsspy.output.Output)
+    fig, ax = sm.plot_pfss(rss=2.5, pfss_solution=pfss_solution, vary=True, return_plot_object=True,
+                           numbered_markers=True, long_sector=[290, 328], long_sector_vsw=[400, 600],
+                           long_sector_color='red')
+    assert isinstance(fig, matplotlib.figure.Figure)
